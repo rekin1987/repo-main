7a743b43b90a53becb58f361062adcfd102379fc
Pawel Suszek <Pawel.Suszek@flir.se>
2017-07-19 13:45:41 +0200

    updated UI

--- PasuBleSample/PasuBleClientSample/src/main/java/pl/emget/pasubleclientsample/CustomPeripheralActivity.java
+++ PasuBleSample/PasuBleClientSample/src/main/java/pl/emget/pasubleclientsample/CustomPeripheralActivity.java
@@ -6,24 +6,32 @@ import android.bluetooth.BluetoothGattService;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.List;
+
+import pl.emget.pasubleserverprofile.ServerProfile;
 
 public class CustomPeripheralActivity extends Activity implements GattConnectionCallback {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final String EXTRAS_DEVICE_NAME = "CONNECTION_ACTIVITY_EXTRAS_DEVICE_NAME";
     public static final String EXTRAS_DEVICE_ADDRESS = "CONNECTION_ACTIVITY_EXTRAS_DEVICE_ADDRESS";
-    private Handler mHandler;
-    private TextView mStatusLabel;
 
-    private TextView mConnectionState;
-    private TextView mDataField;
+    private Handler mUiHandler;
+    private TextView mStatusLabel;
+    private TextView mManufacturerLabel;
+    private TextView mTemperatureLabel;
+    private TextView mVoltageLabel;
+
     private String mDeviceName;
     private String mDeviceAddress;
+    private String mManufacturerName;
+    private String mFirmwareVersion;
 
     private GattConnectionWrapper mGattConnectionWrapper;
+    private BluetoothGattCharacteristic mColorSetCharacteristic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +39,7 @@ public class CustomPeripheralActivity extends Activity implements GattConnection
         Log.d(TAG, "onCreate()");
         setContentView(R.layout.activity_custom_peripheral);
 
-        mHandler = new Handler();
+        mUiHandler = new Handler();
         mDeviceName = getIntent().getStringExtra(EXTRAS_DEVICE_NAME);
         mDeviceAddress = getIntent().getStringExtra(EXTRAS_DEVICE_ADDRESS);
         mGattConnectionWrapper = new GattConnectionWrapper(this, this, mDeviceAddress);
@@ -42,34 +50,104 @@ public class CustomPeripheralActivity extends Activity implements GattConnection
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        mGattConnectionWrapper.shutdown();
     }
 
     private void setupUI() {
-        // Sets up UI references.
-        ((TextView) findViewById(R.id.device_address)).setText(mDeviceAddress);
-        //        mConnectionState = (TextView) findViewById(R.id.connection_state);
-        //        mDataField = (TextView) findViewById(R.id.data_value);
-
+        ((TextView) findViewById(R.id.deviceDetails)).setText(String.format(getString(R.string.device_details_label), mDeviceName, mDeviceAddress));
+        mStatusLabel = (TextView) findViewById(R.id.statusLabel);
+        mManufacturerLabel = (TextView) findViewById(R.id.manufacturerLabel);
+        mTemperatureLabel = (TextView) findViewById(R.id.temperatureLabel);
+        mVoltageLabel = (TextView) findViewById(R.id.voltageLabel);
         getActionBar().setTitle(mDeviceName);
     }
 
     @Override
-    public void postStatusUpdate(String status) {
+    public void postStatusUpdate(final String status) {
         Log.d(TAG, "postStatusUpdate()");
+        mUiHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mStatusLabel.setText(status);
+            }
+        });
     }
 
     @Override
     public void onServicesDiscovered(List<BluetoothGattService> services) {
         Log.d(TAG, "onServicesDiscovered()");
+        for (BluetoothGattService service : services) {
+            if (service.getUuid().equals(ServerProfile.DEVICE_INFO_SERVICE_UUID)) {
+                List<BluetoothGattCharacteristic> characteristics = service.getCharacteristics();
+                for (BluetoothGattCharacteristic ch : characteristics) {
+                    mGattConnectionWrapper.readCharacteristic(ch);
+                }
+            } else if (service.getUuid().equals(ServerProfile.CUSTOM_METRICS_SERVICE_UUID)) {
+                List<BluetoothGattCharacteristic> characteristics = service.getCharacteristics();
+                for (BluetoothGattCharacteristic ch : characteristics) {
+                    if (ch.getUuid().equals(ServerProfile.CUSTOM_METRICS_CHARACTERISTIC_TEMPERATURE_UUID)) {
+                        mGattConnectionWrapper.setCharacteristicNotification(ch, true);
+                    } else if (ch.getUuid().equals(ServerProfile.CUSTOM_METRICS_CHARACTERISTIC_VOLTAGE_UUID)) {
+                        mGattConnectionWrapper.setCharacteristicNotification(ch, true);
+                    } else if (ch.getUuid().equals(ServerProfile.CUSTOM_METRICS_CHARACTERISTIC_COLOR_UUID)) {
+                        mColorSetCharacteristic = ch;
+                    }
+                }
+            }
+        }
     }
 
     @Override
     public void onCharacteristicRead(BluetoothGattCharacteristic characteristic) {
         Log.d(TAG, "onCharacteristicRead()");
+        if (characteristic.getUuid().equals(ServerProfile.DEVICE_INFO_CHARACTERISTIC_MANUFACTURER_NAME_UUID)) {
+            mManufacturerName = new String(characteristic.getValue());
+        } else if (characteristic.getUuid().equals(ServerProfile.DEVICE_INFO_CHARACTERISTIC_FIRMWARE_VERSION_UUID)) {
+            mFirmwareVersion = new String(characteristic.getValue());
+        }
+        if (mManufacturerName != null && mFirmwareVersion != null) {
+            mUiHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    mManufacturerLabel.setText(String.format(getString(R.string.firmware_label), mManufacturerName, mFirmwareVersion));
+                }
+            });
+        }
     }
 
     @Override
-    public void onCharacteristicNotification(BluetoothGattCharacteristic characteristic) {
+    public void onCharacteristicNotification(final BluetoothGattCharacteristic ch) {
         Log.d(TAG, "onCharacteristicNotification()");
+        if (ch.getUuid().equals(ServerProfile.CUSTOM_METRICS_CHARACTERISTIC_TEMPERATURE_UUID)) {
+            mUiHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    mTemperatureLabel.setText(String.format(getString(R.string.temperature_label), new String(ch.getValue())));
+                }
+            });
+        } else if (ch.getUuid().equals(ServerProfile.CUSTOM_METRICS_CHARACTERISTIC_VOLTAGE_UUID)) {
+            mUiHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    mVoltageLabel.setText(String.format(getString(R.string.voltage_label), new String(ch.getValue())));
+                }
+            });
+        }
     }
+
+    public void onConnectClick(View view) {
+        if (!mGattConnectionWrapper.isConnected()) {
+            mGattConnectionWrapper.connect();
+        }
+    }
+
+    public void onColorChangeClick(View view) {
+        ++counter;
+        if(counter > 2){
+            counter = 0;
+        }
+        mGattConnectionWrapper.writeCharacteristic(mColorSetCharacteristic, String.valueOf(counter));
+    }
+
+    int counter = 0;
 }
