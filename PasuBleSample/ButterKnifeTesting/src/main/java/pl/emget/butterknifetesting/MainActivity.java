e59932678e54e45081401d8f0bc24987974f9ac5
Pawel Suszek <Pawel.Suszek@flir.se>
2017-07-25 14:17:17 +0200

    added ButterKnife annotations

--- PasuBleSample/ButterKnifeTesting/src/main/java/pl/emget/butterknifetesting/MainActivity.java
+++ PasuBleSample/ButterKnifeTesting/src/main/java/pl/emget/butterknifetesting/MainActivity.java
@@ -2,12 +2,26 @@ package pl.emget.butterknifetesting;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends Activity {
+
+    @BindView(R.id.text1)
+    TextView textView1;
+    @BindView(R.id.button1)
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        textView1.setText("KKKK");
     }
 }
