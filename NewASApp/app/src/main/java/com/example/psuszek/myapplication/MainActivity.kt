8aa83016772818b82d87ee3f6233ea01b023491b
Pawel Suszek <Pawel.Suszek@flir.se>
2017-07-26 14:15:16 +0200

    added kotlin project with butterknife dependency

---
+++ NewASApp/app/src/main/java/com/example/psuszek/myapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.psuszek.myapplication
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
+
+
+class MainActivity : Activity() {
+
+    //val title: TextView by bindView(R.id.text1)
+
+    var aa: Button?=null
+
+    //@BindView(R.id.text1) public var title: TextView? = null
+
+    @BindView(R.id.text1)
+    lateinit var title2 : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        savedInstanceState.toString()
+
+        ButterKnife.bind(this)
+
+        //title = findViewById(R.id.seekBar1)
+
+        title2.setText("kkk");
+    }
+}
