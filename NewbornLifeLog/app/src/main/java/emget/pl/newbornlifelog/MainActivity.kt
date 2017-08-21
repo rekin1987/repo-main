18432b10f4e91f158c1c687d621dbbeb033409be
Pawel Suszek <pawel.suszek2@gmail.com>
2017-08-21 19:58:13 +0200

    added Anko library

--- NewbornLifeLog/app/src/main/java/emget/pl/newbornlifelog/MainActivity.kt
+++ NewbornLifeLog/app/src/main/java/emget/pl/newbornlifelog/MainActivity.kt
@@ -2,11 +2,28 @@ package emget.pl.newbornlifelog
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.button
+import org.jetbrains.anko.editText
+import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.verticalLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // where text1 is a TextView with ID 'text1'
+        text1.text = "aaa"
+
+        verticalLayout {
+            val name = editText()
+            button("Say Hello") {
+                onClick { toast("Hello, ${name.text}!") }
+            }
+        }
+
     }
 }
